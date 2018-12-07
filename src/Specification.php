32cbb31ed68c3e2caa2016ac43de25bb320a344b
jlttt <jlttt@laposte.net>
2018-12-07 09:51:46 +0100

    second test passes

--- src/Specification.php
+++ src/Specification.php
@@ -6,11 +6,17 @@ namespace jlttt\Specify;
 
 final class Specification
 {
+    private $key;
+
+    public function __construct($key) {
+        $this->key = $key;
+    }
+
     /**
      * @param mixed $candidate
      * @return boolean
      */
     public function isSatisfiedBy($candidate) {
-        return true;
+        return isset($candidate[$this->key]);
     }
 }
--- tests/SpecificationTest.php
+++ tests/SpecificationTest.php
@@ -6,7 +6,13 @@ use jlttt\Specify\Specification;
 final class SpecificationTest extends TestCase {
     public function testSatisfiedBySimpleCandidate() {
         $specification = new Specification('foo', 'bar');
-        $candidate = ['foo', 'bar'];
+        $candidate = ['foo' => 'bar'];
         $this->assertTrue($specification->isSatisfiedBy($candidate));
+    }
+
+    public function testNotSatisfiedBySimpleCandidateWithDifferentKey() {
+        $specification = new Specification('foo', 'bar');
+        $candidate = ['baz' => 'bar'];
+        $this->assertFalse($specification->isSatisfiedBy($candidate));
     }
 }
